e48bada64afcac69ef49f0ceb5a8b320a9ca8be8
Yury Stolarz <yurysscareer@gmail.com>
2023-04-04 16:42:51 -0300

    feat: add req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass
+    new_product = Product(
+        9,
+        'Testing',
+        'Test',
+        '2023-04-03',
+        '2030-04-03',
+        '2UA449P',
+        'deve passar'
+    )
+
+    assert new_product.id == 9
+    assert new_product.nome_do_produto == 'Testing'
+    assert new_product.nome_da_empresa == 'Test'
+    assert new_product.data_de_fabricacao == '2023-04-03'
+    assert new_product.data_de_validade == '2030-04-03'
+    assert new_product.numero_de_serie == '2UA449P'
+    assert new_product.instrucoes_de_armazenamento == 'deve passar'
