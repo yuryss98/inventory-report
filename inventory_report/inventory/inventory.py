ee49b8990ff4f980ee83900544fa9d8ecf66ee54
Yury Stolarz <yurysscareer@gmail.com>
2023-04-05 18:14:28 -0300

    refactor: refactoring inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,41 +1,20 @@
 from inventory_report.reports import simple_report, complete_report
-import csv
-import json
-import xmltodict
+from inventory_report.importer import csv_importer, json_importer, xml_importer
 
 
 class Inventory:
-    @classmethod
-    def open_json(cls, path):
-        with open(path) as file:
-            return json.load(file)
-
-    @classmethod
-    def open_csv(cls, path):
-        with open(path, "r") as file:
-            report_reader = csv.DictReader(file)
-
-            return list(report_reader)
-
-    @classmethod
-    def open_xml(cls, path):
-        with open(path) as file:
-            report_reader = xmltodict.parse(file.read())
-
-            return report_reader["dataset"]["record"]
-
     @classmethod
     def import_data(cls, path, type_report):
         reports = []
 
         if path.lower().endswith("csv"):
-            reports = cls.open_csv(path)
+            reports = csv_importer.CsvImporter.import_data(path)
         elif path.lower().endswith("xml"):
-            reports = cls.open_xml(path)
+            reports = xml_importer.XmlImporter.import_data(path)
         else:
-            reports = cls.open_json(path)
+            reports = json_importer.JsonImporter.import_data(path)
 
         if type_report == "simples":
-            return simple_report.SimpleReport.generate(list(reports))
+            return simple_report.SimpleReport.generate(reports)
         else:
-            return complete_report.CompleteReport.generate(list(reports))
+            return complete_report.CompleteReport.generate(reports)
