012a3d50d0829995e6b1c5f4e7651f869e40602f
Yury Stolarz <yurysscareer@gmail.com>
2023-04-05 19:21:19 -0300

    feat: add req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        9,
+        'Coca-cola',
+        'Coca-cola',
+        '2023-01-03',
+        '2023-05-03',
+        '5449000000996',
+        'na geladeira'
+    )
+
+    assert new_product.__repr__() == (
+        "O produto Coca-cola"
+        " fabricado em 2023-01-03"
+        " por Coca-cola com validade"
+        " até 2023-05-03"
+        " precisa ser armazenado na geladeira."
+    )
